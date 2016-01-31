4745e8504f7946aac24562a13d1f770f4502aa6e
iischarizard <zlnorman10@gmail.com>
2016-01-30 23:32:11 -0800

    Update Sprites.java

--- src/Sprites.java
+++ src/Sprites.java
@@ -1,4 +1,23 @@
 
 public class Sprites {
 
+	//Trump's stuff
+	public static String TRUMPFILEPATH = "resources/trump.jpg";
+	//THE DIMENSIONS OF YOUR IMAGE
+	public static int TRUMPIMAGEDIMENSION[] = {356, 250};
+	//THE DIMENSIONS OF HOW BIG YOU WANT THE INGAME THING TO BE
+	public static int TRUMPGAMEDIMENSION[] = {75, 50};
+	
+	//Clinton's stuff
+	public static String CLINTONFILEPATH = "resources/clinton.jpg";
+	//THE DIMENSIONS OF YOUR IMAGE
+	public static int CLINTONIMAGEDIMENSIONS[] = {2070, 2588};
+	//THE DIMENSIONS OF HOW BIG YOU WANT THE INGAME THING TO BE
+	public static int CLINTONGAMEDIMENSIONS[] = {100, 150};
+
+	public static String BACKGROUNDFILEPATH = "resources/trumpxclinton.jpg";
+	//THE DIMENSIONS OF YOUR IMAGE
+	public static int BACKGROUNDIMAGEDIMENSIONS[] = {614, 412};
+	
 }
+
